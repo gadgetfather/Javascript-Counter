64bb29ff29bb2be52aa4a77ad21d408c53954229
Akshay Kamble <kamblea635@gmail.com>
2021-12-24 11:55:07 +0530

    added function for countdown

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const countdown = ()=>{
     const countDate = new Date ("May 6,2022 00:00:00").getTime()
     const currentDate = new Date().getTime()
     let gap = countDate- currentDate
-    console.log(gap)
+    // console.log(gap)
+
+    const second = 1000;
+    const minute = second*60;
+    const hour = minute *60;
+    const day = hour*24;
+
+    const textDay = Math.floor(gap/day)
+    const textHour = Math.floor((gap%day)/hour)
+    const textMin = Math.floor((gap%hour)/minute)
+    const textsecond = Math.floor((gap%minute)/second)
+    
+
+    document.querySelector(".day").textContent = textDay
+    document.querySelector(".hour").textContent = textHour
+    document.querySelector(".minute").textContent = textMin
+    document.querySelector(".second").textContent = textsecond
 }
 countdown()
